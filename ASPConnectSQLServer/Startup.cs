c99a961880f92c72579fb1d582097c8c513b3076
namchokGithub <pech4751@gmail.com>
2020-08-03 17:03:53 +0700

    change home page

--- ASPConnectSQLServer/Startup.cs
+++ ASPConnectSQLServer/Startup.cs
@@ -50,7 +50,7 @@ namespace ASPConnectSQLServer
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=First}/{action=Index}/{id?}");
             });
         }
     }
