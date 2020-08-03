52de90e9afef6fde7a89582423d20188a523b45f
namchokGithub <pech4751@gmail.com>
2020-08-03 10:39:15 +0700

    add first controller

--- ASPConnectSQLServer/Controllers/FirstController.cs
+++ ASPConnectSQLServer/Controllers/FirstController.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
 namespace ASPConnectSQLServer.Controllers
 {
     public class FirstController : Controller
     {
+        private readonly IConfiguration configuration;
+
+        public FirstController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+
         public IActionResult Index()
         {
+            string connnectionstring = configuration.GetConnectionString("DefaultConnectionString");
+
+            SqlConnection connection = new SqlConnection("connnectionstring");
+
+            connection.Open();
+
+            SqlCommand com = new SqlCommand("Select count(*) from account", connection);
+            var count = (int)com.ExecuteScalar();
+
+            ViewData["TotalData"] = count;
+
+            connection.Close();
+
             return View();
         }
     }
